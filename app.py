4816094bb42caf6adcaa5caa29ff89fd5a1e9dc2
2210344 <2210344@ecc.ac.jp>
2023-02-15 15:06:42 +0900

    コミット

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('logout')
+def logout():
+    return render_template("logout.html")
