433475499b3e72b72ebe6e5582e8623346d68d11
sintaro.k <sintaro.katuta@gmail.com>
2023-02-21 00:29:22 +0900

    位置とか色はテキトーやけど完成

--- static/js/gacha.js
+++ static/js/gacha.js
@@ -4,6 +4,7 @@ const gatya_content = document.querySelector('#gatya-content');
 const gatya_tabel = document.querySelector('#gatya-table');
 const remove = document.querySelectorAll('.remove');
 const probability = Array.from(document.querySelectorAll(".probability"));
+let count = 0;
 
 
 gatya_form.addEventListener('submit', (element) =>{
@@ -73,6 +74,111 @@ probability.forEach((p,index) =>{
     })
 })
 
+//canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+// 棒の色が変わらないので円を最初に描画
+
+// 回す所の円を描画
+ctx.beginPath();
+ctx.arc( 150, 300, 80, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+ctx.fillStyle = "#c9caca";
+ctx.fill();
+ctx.stroke();
+
+// 円の中の棒を描画
+ctx.beginPath();
+ctx.rect(70, 290, 160, 20 );
+ctx.fillStyle = "#fff";
+ctx.fill();
+ctx.stroke();
+
+// 上部のカプセルの枠を描画
+createRoundRect(ctx, 30, 30, 350, 150, 20);
+ctx.fillStyle = "#fff";
+ctx.fill();
+ctx.stroke();
+
+// 下部のカプセルの出口を描画
+createRoundRect(ctx, 280, 350, 100, 100, 20);
+ctx.fillStyle = "#333";
+ctx.fill();
+ctx.stroke();
+
+
+let degree = [15,30,45,60,75,90,105,120,135,150,165,180,195,210,225,240,255,270,285,300,315,330,345,360];// 角度の配列
+
+function spin(){
+    window.setTimeout(function() {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+
+        // 中央からランダムに回転
+        ctx.beginPath();
+        ctx.translate(150,300);// ctx.translate(x,y) は ctx.arc(x,y)と同じ値
+        ctx.rotate(degree[Math.floor(Math.random()*degree.length)]*Math.PI / 360);
+        ctx.translate(-150,-300);
+
+        // 回す所の円を描画
+        ctx.arc( 150, 300, 80, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+        ctx.fillStyle = "#c9caca";
+        ctx.fill();
+        ctx.stroke();
+
+        // 円の中の棒を描画
+        ctx.beginPath();
+        ctx.rect(70, 290, 160, 20 );
+        ctx.fillStyle = "#fff";
+        ctx.fill();
+        ctx.stroke();
+
+        // 位置と回転をリセット
+        ctx.setTransform(1,0,0,1,0,0);
+
+        // 上部のカプセルの枠を描画
+        createRoundRect(ctx, 30, 30, 350, 150, 20);
+        ctx.fillStyle = "#fff";
+        ctx.fill();
+        ctx.stroke();
+        
+        // 下部のカプセルの出口を描画
+        createRoundRect(ctx, 280, 350, 100, 100, 20);
+        ctx.fillStyle = "#333";
+        ctx.fill();
+        ctx.stroke();
+
+    },500);
+    ctx.restore();
+};
+
+canvas.addEventListener('click',(e)=>{
+    // マウスの座標をCanvas内の座標とあわせるため
+    const rect = canvas.getBoundingClientRect();
+    const point = {
+        x:e.clientX - rect.left,
+        y:e.clientY - rect.top
+    };
+    //クリック判定処理
+    const square = {
+        x:150,y:300,
+        r:80
+    };
+
+    const hit =
+        Math.pow(square.x - point.x, 2) + Math.pow(square.y - point.y, 2) <= Math.pow(square.r, 2);
+
+    if (hit){
+        // 二回クリックしたら画面遷移
+        if(count < 2){
+            for(let i=0;i<50;i++){
+                spin();
+            }
+        }else{
+            //alert("!!!!");
+        }
+        count++;// クリックしたカウント
+    }
+});
 
 function remove_element(element) {
     element.parentElement.parentElement.remove();
@@ -82,65 +188,26 @@ function show_probability(element) {
     element.parentElement.nextElementSibling.innerHTML = element.value + "%";
 }
 
-//canvas
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
-
-ctx.fillRect(100,250,150,50);
-var degree = 0;
-
-function spin(){
-    for(let i=0;i<5;i++){
-        window.setTimeout(function() {
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-            //ctx.save();
-                ctx.beginPath();
-            // ctx.translate(canvas.width/2,canvas.height/2);
-                ctx.translate(canvas.width/2.35,canvas.height/1.45);
-                //ctx.rotate(Math.PI /180);
-                ctx.rotate(45*Math.PI /360);
-                ctx.translate(-canvas.width/2.35, -canvas.height/1.45);
-                ctx.fillRect(100,250,150,50);   
-
-        },10);
-        
-    }
-
-    ctx.restore();
-
-//    requestAnimationFrame(spin)
- 
-};
-canvas.addEventListener('click',(e)=>{
-
-    spin();
-});
-
-//canvas
-// const canvas = document.getElementById("canvas");
-// const ctx = canvas.getContext('2d');
-
-// ctx.fillRect(100,250,150,50);
-// var degree = 0;
-
-// function spin(){
-// setInterval( function(){
-//     ctx.save();
-
-//     ctx.beginPath();
-//     ctx.clearRect(0,0,canvas.width,canvas.height);
-
-//     // ctx.translate(canvas.width/2,canvas.height/2);
-//     ctx.translate(canvas.width/2.35,canvas.height/1.45);
-//     ctx.rotate(++degree * Math.PI /180);
-//     ctx.translate(-canvas.width/2.35, -canvas.height/1.45);
-
-//     ctx.fillRect(100,250,150,50);
-
-//     ctx.restore();
- 
-// },25);
-// }
-// canvas.addEventListener('click',(e)=>{
-//     spin();
-// });
+function createRoundRect(context, x, y, width, height, radius){
+    ctx.beginPath();
+    // 左上移動
+    ctx.moveTo(x + radius, y);
+    // 右上に繋がる線
+    ctx.lineTo(x + width - radius, y);
+    // 弧
+    ctx.arcTo(x + width, y, x + width, y + radius, radius);
+    // 右下に繋がる線
+    ctx.lineTo(x + width, y + height - radius);
+    // 弧
+    ctx.arcTo(x + width, y + height, x + width - radius 
+        , y + height, radius);
+    // 左下に繋がる線
+    ctx.lineTo(x + radius, y + height); 
+    // 弧
+    ctx.arcTo(x, y + height, x, y + height - radius, radius);
+    // 右上に繋がる線
+    ctx.lineTo(x, y + radius);
+    // 弧
+    ctx.arcTo(x, y, x + radius, y, radius);
+    ctx.closePath();
+}
